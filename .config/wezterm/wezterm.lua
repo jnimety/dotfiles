1e8212ed6f3aec178f4da61a5491098d4b97bc7f
Joel Nimety <jnimety@gmail.com>
2025-10-11 17:45:43 -0500

    cleanup wezterm config. Note: WebGpu is Mac only

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,30 +2,27 @@ local wezterm = require("wezterm")
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function(cmd)
-  local tab, _pane, window = mux.spawn_window(cmd or {})
+  local _, _, window = mux.spawn_window(cmd or {})
   window:gui_window():maximize()
 end)
 
 return {
-  front_end = "WebGpu",
   audible_bell = "Disabled",
   color_scheme = "tokyonight-night",
   font = wezterm.font_with_fallback({
     { family = "Iosevka Term", stretch = "Expanded" },
-    -- 'Source Code Pro',
     "Symbols Nerd Font Mono",
   }),
   font_rules = {
     {
       intensity = "Bold",
       font = wezterm.font("Iosevka Term", { weight = "Medium", stretch = "Expanded" }),
-      -- font = wezterm.font('Source Code Pro', { weight = "Medium" }),
     },
   },
   font_size = 16.0,
   harfbuzz_features = { "calt=0" },
   hide_tab_bar_if_only_one_tab = true,
-  -- window_background_opacity = 0.95,
+  window_background_opacity = 0.87,
   selection_word_boundary = " \t\n{}[]()\"'`,;:", -- default: " \t\n{}[]()\"'`"
   window_decorations = "RESIZE",
   window_frame = {
