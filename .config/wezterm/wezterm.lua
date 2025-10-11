7447022dd08b13faa617f4700d7ae950481350d8
Joel Nimety <jnimety@gmail.com>
2025-10-11 17:52:42 -0500

    not needed, window tiling manager does this

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,11 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 
-wezterm.on("gui-startup", function(cmd)
-  local _, _, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
-end)
+-- not needed if your window tiling manager does it for you
+-- wezterm.on("gui-startup", function(cmd)
+--   local _, _, window = mux.spawn_window(cmd or {})
+--   window:gui_window():maximize()
+-- end)
 
 return {
   audible_bell = "Disabled",
