907e0b21f6081751f19d95e7a2e5eba8c9df1f00
Joel Nimety <jnimety@gmail.com>
2025-10-13 13:22:26 -0500

    update lua_ls config per the lspconfig documentation for *much* faster load times

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -311,13 +311,39 @@ return {
       --   },
       -- })
 
-      -- Make runtime files discoverable to the server
-      local runtime_path = vim.split(package.path, ";")
-      table.insert(runtime_path, "lua/?.lua")
-      table.insert(runtime_path, "lua/?/init.lua")
-
       vim.lsp.config("lua_ls", {
         capabilities = capabilities,
+        on_init = function(client)
+          if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if
+              path ~= vim.fn.stdpath("config")
+              and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+            then
+              return
+            end
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+                -- Depending on the usage, you might want to add additional paths here.
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
+              },
+              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+              -- library = vim.api.nvim_get_runtime_file("", true)
+            },
+          })
+        end,
         settings = {
           Lua = {
             format = {
@@ -329,21 +355,10 @@ return {
                 indent_size = "2",
               },
             },
-            runtime = {
-              -- Tell the language server which version of Lua you're using (most likely LuaJIT)
-              version = "LuaJIT",
-              -- Setup your lua path
-              path = runtime_path,
-            },
             diagnostics = {
-              globals = { "vim" },
               neededFileStatus = {
                 ["codestyle-check"] = "Any",
               },
-            },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
             },
             -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = { enable = false },
