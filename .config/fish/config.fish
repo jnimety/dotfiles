bc372c34e4e6a5af0f77deb8a7f55d985929213d
Joel Nimety <jnimety@gmail.com>
2025-10-15 17:24:15 -0500

    whitespace

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,66 +30,66 @@ set -Ux LESSCHARSET 'utf-8'
 # set -Ux LESS_TERMCAP_us (set_color e0af68) # begin underline
 
 function bind_bang
-    switch (commandline -t)[-1]
-        case "!"
-            commandline -t -- $history[1]
-            commandline -f repaint
-        case "*"
-            commandline -i !
-    end
+  switch (commandline -t)[-1]
+  case "!"
+    commandline -t -- $history[1]
+    commandline -f repaint
+  case "*"
+    commandline -i !
+  end
 end
 
 function bind_dollar
-    switch (commandline -t)[-1]
-        # Variation on the original, vanilla "!" case
-        # ===========================================
-        #
-        # If the `!$` is preceded by text, search backward for tokens that
-        # contain that text as a substring. E.g., if we'd previously run
-        #
-        #   git checkout -b a_feature_branch
-        #   git checkout main
-        #
-        # then the `fea!$` in the following would be replaced with
-        # `a_feature_branch`
-        #
-        #   git branch -d fea!$
-        #
-        # and our command line would look like
-        #
-        #   git branch -d a_feature_branch
-        #
-        case "*!"
-            commandline -f backward-delete-char history-token-search-backward
-        case "*"
-            commandline -i '$'
-    end
+  switch (commandline -t)[-1]
+    # Variation on the original, vanilla "!" case
+    # ===========================================
+    #
+    # If the `!$` is preceded by text, search backward for tokens that
+    # contain that text as a substring. E.g., if we'd previously run
+    #
+    #   git checkout -b a_feature_branch
+    #   git checkout main
+    #
+    # then the `fea!$` in the following would be replaced with
+    # `a_feature_branch`
+    #
+    #   git branch -d fea!$
+    #
+    # and our command line would look like
+    #
+    #   git branch -d a_feature_branch
+    #
+  case "*!"
+    commandline -f backward-delete-char history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
 end
 
 # enables $?
 function bind_status
-    commandline -i (echo '$status')
+  commandline -i (echo '$status')
 end
 
 
 # enables $$
 function bind_self
-    commandline -i (echo '$fish_pid')
+  commandline -i (echo '$fish_pid')
 end
 
 # enable keybindings
 function fish_user_key_bindings
-    # Set the cursor shapes for the different vi modes.
-    set fish_cursor_default block blink
-    set fish_cursor_insert line blink
-    # set fish_cursor_replace_one underscore blink
-    set fish_cursor_visual block
+  # Set the cursor shapes for the different vi modes.
+  set fish_cursor_default block blink
+  set fish_cursor_insert line blink
+  # set fish_cursor_replace_one underscore blink
+  set fish_cursor_visual block
 
-    fish_vi_key_bindings
+  fish_vi_key_bindings
 
-    bind '$?' bind_status
-    bind '$$' bind_self
+  bind '$?' bind_status
+  bind '$$' bind_self
 
-    bind -M insert ! bind_bang
-    bind -M insert '$' bind_dollar
+  bind -M insert ! bind_bang
+  bind -M insert '$' bind_dollar
 end
