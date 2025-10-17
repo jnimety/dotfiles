02173a460ef5b247ca70d598719ef2f9c0eefb8f
Joel Nimety <jnimety@gmail.com>
2025-10-16 20:34:07 -0500

    fix broken highlight colors because I globally changed colors.fg_gutter

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -7,45 +7,23 @@ return {
     ---@field on_colors fun(colors: ColorScheme)
     ---@field on_highlights fun(highlights: tokyonight.Highlights, colors: ColorScheme)
     opts = {
-      dim_inactive = false,
       style = "night",
+
       styles = {
         sidebars = "transparent",
         floats = "tranparent",
       },
       transparent = true,
 
-      on_colors = function(colors)
-        colors.fg_gutter = colors.fg_dark
-      end,
-
       ---@param highlights tokyonight.Highlights
       ---@param colors ColorScheme
       on_highlights = function(highlights, colors)
-        highlights.CursorLine = { bg = colors.bg }
-        highlights.CursorLineNr = { fg = colors.yellow, bg = colors.bg }
-        highlights.LineNr = { fg = colors.yellow }
-        highlights.LspReferenceRead = { bg = colors.bg_highlight }
-        highlights.LspReferenceText = { bg = colors.bg_highlight }
-        highlights.LspReferenceWrite = { bg = colors.bg_highlight }
-        highlights.WinSeparator = { fg = colors.yellow }
+        highlights.CursorLine.bg = colors.bg
+        highlights.CursorLineNr.bg = colors.bg
+        highlights.LineNr.fg = colors.yellow
+        highlights.LineNrAbove.fg = colors.fg_dark
+        highlights.LineNrBelow.fg = colors.fg_dark
       end,
-
-      cache = true,
-
-      ---@type table<string, boolean|{enabled:boolean}>
-      plugins = {
-        -- enable all plugins when not using lazy.nvim
-        -- set to false to manually enable/disable plugins
-        all = package.loaded.lazy == nil,
-        -- uses your plugin manager to automatically enable needed plugins
-        -- currently only lazy.nvim is supported
-        auto = true,
-        -- add any plugins here that you want to enable
-        -- for all possible plugins, see:
-        --   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
-        -- telescope = true,
-      },
     },
     config = function(_, opts)
       require("tokyonight").setup(opts)
